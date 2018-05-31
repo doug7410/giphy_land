f74e1197ce7d8c17e1ca20d43eb536e3e31e8a4d
Doug Steinberg <d.steinberg79@gmail.com>
2018-05-30 22:37:21 -0400

    add vuex and create store

---
+++ src/store/index.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import giphs from './modules/giphs'
+import search from './modules/search'
+import favorites from './modules/favorites'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  modules: {
+    giphs,
+    search,
+    favorites
+  }
+})
