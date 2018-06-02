b81d8f20a3319a42a6de708461f9b68691b08fe2
Doug Steinberg <d.steinberg79@gmail.com>
2018-06-02 13:03:55 -0400

    add vue-masonry plugin

--- src/components/GiphyGrid.vue
+++ src/components/GiphyGrid.vue
@@ -1,6 +1,12 @@
 <template>
-  <div>
-    <giphy-card v-for="giph in giphs" :key="giph.id" :giph="giph"/>
+  <div v-masonry transition-duration="0.3s" item-selector=".item">
+    <giphy-card
+      v-for="giph in giphs"
+      :key="giph.id"
+      :giph="giph"
+      v-masonry-tile
+      class="item"
+    />
   </div>
 
 </template>
@@ -8,6 +14,10 @@
 <script>
   import GiphyCard from './GiphyCard'
   import { mapGetters } from 'vuex'
+  import Vue from 'vue';
+  import { VueMasonryPlugin } from 'vue-masonry';
+
+  Vue.use(VueMasonryPlugin);
 
   export default {
     name: "giphy-grid",
